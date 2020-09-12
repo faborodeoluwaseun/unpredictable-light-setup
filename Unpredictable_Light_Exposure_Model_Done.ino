7d5b2ecc8703393b601b9d791ccf0a0918702f2c
faborodeoluwaseun <38437683+faborodeoluwaseun@users.noreply.github.com>
2020-09-12 11:31:51 +0200

    Add files via upload

---
+++ Unpredictable_Light_Exposure_Model_Done.ino
@@ -0,0 +1,21 @@
+
+int WhiteLED = 3;
+int brightness = 35;    // how bright the LED is
+
+void setup( )
+{
+  pinMode( WhiteLED, OUTPUT) ; //set LEDPin to be an OUTPUT
+  Serial.begin(9600);      // turn on Serial Port
+}
+
+void loop( ){
+
+analogWrite(WhiteLED, brightness); // put on the LED with a brightness of the value indicated above
+delay(random(120000,3600000)); // wait for this period of time
+
+digitalWrite (WhiteLED, LOW); // turn LED off
+delay(random(120000,3600000)); //wait for this period of time
+
+}
+
+
